68ec4004e10a88dc2676c3ae1dc780110f243d24
Bert Vanderbauwhede <batlock666@gmail.com>
2013-07-13 23:12:14 +0200

    Add function safename_decode

--- src/fnord/safename/codec.py
+++ src/fnord/safename/codec.py
@@ -16,6 +16,21 @@ _safename_encode_chain = HandlerChain(
         lambda string: "(%s)" % hex(ord(string))[2:]),
 )
 
+_safename_decode_chain = HandlerChain(
+    Handler(
+        "\{[a-z]+\}",
+        lambda string: string[1:-1].upper()),
+    Handler(
+        "_+",
+        lambda string: string.replace("_", " ")),
+    Handler(
+        "[a-z0-9\-+!$%&'@~#.,^]+",
+        lambda string: string),
+    Handler(
+        "\([0-9a-f]+\)",
+        lambda string: unichr(int(string[1:-1], 16))),
+)
+
 
 def safename_encode(string, errors="strict"):
     """Encode a string with codec ``safename``.
@@ -27,3 +42,15 @@ def safename_encode(string, errors="strict"):
         return _safename_encode_chain(string), len(string)
     except UnicodeError as error:
         raise UnicodeEncodeError(error.args[0])
+
+
+def safename_decode(string, errors="strict"):
+    """Decode a string with codec ``safename``.
+    """
+    if errors != "strict":
+        raise UnicodeError(u"Unsupported error handling: %s" % errors)
+
+    try:
+        return _safename_decode_chain(string), len(string)
+    except UnicodeError as error:
+        raise UnicodeDecodeError(error.args[0])
