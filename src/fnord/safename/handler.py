a9e1ad5627b8373285609873af0d016b2bf4656d
Bert Vanderbauwhede <batlock666@gmail.com>
2013-07-13 22:41:13 +0200

    Add class Handler

--- src/fnord/safename/handler.py
+++ src/fnord/safename/handler.py
@@ -1,1 +1,23 @@
-#
+import re
+
+
+class Handler(object):
+    """String-handler.
+    """
+
+    def __init__(self, regexp, handle):
+        """Constructor.
+        """
+        self.regexp = re.compile("(?P<head>%s)(?P<tail>.*)" % regexp)
+        self.handle = handle
+
+    def __call__(self, string):
+        """Callable.
+        """
+        match = self.regexp.match(string)
+        if match is None:
+            return "", string
+        else:
+            head = self.handle(match.group("head"))
+            tail = match.group("tail")
+            return head, tail
